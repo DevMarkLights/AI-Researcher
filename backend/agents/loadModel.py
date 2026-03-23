e95da1056f5b1f85a6f99f3c607e03ade9016918
Mark Lights <mlights3@gatech.edu>
2026-03-23 16:26:13 -0400

    up

--- backend/agents/loadModel.py
+++ backend/agents/loadModel.py
@@ -9,7 +9,7 @@ load_dotenv()
 llm_small = None
 llm_large = None
 
-if os.getenv("USE_LOCAL"):
+if os.getenv("USE_LOCAL") == 'true':
     print('local model')
     llm_small = ChatOllama(model="llama3.2:3b", temperature=0)
     llm_large = ChatOllama(model="llama3.2:3b", temperature=0)
